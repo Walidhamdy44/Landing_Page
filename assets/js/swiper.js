319415261110d9a708a74c9f97d6dde0a864fd72
walid hamdy44 <walidhamdy314@gmail.com>
2024-07-06 01:11:50 +0300

    update the code

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,16 +1,24 @@
+// Initialize Swiper
 const swiper = new Swiper(".swiper", {
+  // Set the direction of the swiper
   direction: "horizontal",
+  // Enable loop mode
   loop: true,
+  // Set the speed of the swiper
   speed: 700,
+  // Enable autoplay with a delay of 3 seconds
   autoplay: {
     delay: 3000,
     disableOnInteraction: false,
   },
+  // Enable pagination
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
+  // Enable grab cursor
   grabCursor: true,
+  // Set the effect of the swiper
   effect: "creative",
   creativeEffect: {
     prev: {
@@ -21,11 +29,16 @@ const swiper = new Swiper(".swiper", {
       translate: ["100%", 0, 0],
     },
   },
+  // Add a slide change transition end event
   on: {
     slideChangeTransitionEnd: function () {
+      // Get the active slide
       const activeSlide = this.slides[this.activeIndex];
+      // Get the box element in the active slide
       const box = activeSlide.querySelector(".box");
+      // Get all the text elements in the box
       const textElements = box.querySelectorAll(".btn,h1, p");
+      // Animate each text element
       textElements.forEach((element) => {
         gsap.from(element, {
           opacity: 0,
